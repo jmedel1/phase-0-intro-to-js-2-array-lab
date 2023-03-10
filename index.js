af5098a49d0a191730caf46052a554f7bb921297
jael <jael.medel30@gmail.com>
2023-03-09 23:41:41 -0600

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat (name) {
+    cats.push("Ralph");
+    console.log(cats);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+    console.log(cats);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    console.log(cats);
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    console.log(cats);
+}
+function appendCat(name) {
+  return [...cats,"Broom"];
+}
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+function removeFirstCat() {
+    return cats.slice(1, 4);
+}
